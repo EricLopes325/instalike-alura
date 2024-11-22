b2630f346f972fc7b4422d81de76596c541c31e0
Eric Lopes da Silva <160548042+EricLopes325@users.noreply.github.com>
2024-11-22 18:44:53 -0300

    Aula 3

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 import express from "express";
+import routes from "./src/config/routes/postsRoutes.js";
+// Importa o framework Express, que é fundamental para criar aplicações web Node.js.
+
 
 const posts = [
+    // Array que simula um banco de dados local, contendo alguns posts de exemplo.
     { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
-    { id: 2, descrição: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descrição: "Gato fazendo yoga", imagem: "https://placecats.com/300/150" },
     { id: 3, descrição: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150" }
-  ];
+];
+
 const app = express();
-app.use (express.json());
+// Cria uma instância do Express, que será o ponto de partida da nossa aplicação.
+routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number (id)
-    })
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id)
-    res.status(200).json(posts[index]);
+    // Inicia o servidor Express na porta 3000 e exibe uma mensagem no console quando o servidor estiver pronto para receber requisições.
 });
---
+++ src/models/postsModel.js
@@ -0,0 +1,14 @@
+import conectarAoBanco from "../config/dbConfig.js"
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+// Estabelece a conexão com o banco de dados, utilizando a string de conexão obtida da variável de ambiente `STRING_CONEXAO`. O resultado da conexão é armazenado na variável `conexao`.
+
+export default async function getTodosPosts() {
+    // Função assíncrona para obter todos os posts do banco de dados.
+    const db = conexao.db("instalike-alura")
+    // Obtém o banco de dados chamado "instalike-alura" a partir da conexão estabelecida.
+    const colecao = db.collection("posts")
+    // Obtém a coleção "posts" dentro do banco de dados.
+    return colecao.find().toArray()
+    // Executa uma consulta para encontrar todos os documentos (posts) na coleção e retorna os resultados como um array.
+}
