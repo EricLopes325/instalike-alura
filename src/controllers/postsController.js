66fe598bce1829c9c48d75de31d2db414c68c14d
Eric Lopes da Silva <160548042+EricLopes325@users.noreply.github.com>
2024-11-25 18:43:51 -0300

    PROBLEMA DE SALVAR SÓ O ID NO BANCO

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -20,19 +20,20 @@ export async function listarPosts(req, res)  {
         }
     }
 
-    export async function uploadImagem(req, res) {
-        const novoPost = {
-            descricao: "",
-            imgUrl: req.file.originalname,
-            alt:""
-        };
-        try {
-            const postCriado = await criarPost(novoPost);
-            const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
-            fs.renameSync(req.file.path, imagemAtualizada)
-            res.status(200).json(novoPost);
-        } catch(erro) {
-            console.log(erro.message);
-            res.status(500).jason({"Erro":"Falha na requisição"})
-        }
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
     }
+}
